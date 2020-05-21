082ffd4a913a21af2049afeaf3def55c6c402414
unknown <luukhanhtung2002@gmail.com>
2020-05-21 21:27:09 +0700

    Connect to firestore

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,14 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
+var serviceAccount = require("../serviceAccountKey.json");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://up-team-d241d.firebaseio.com/"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+const db = admin.firestore();
+
