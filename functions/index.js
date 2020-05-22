cb50dd280526cfd912a10701ebdce37e0942577d
unknown <luukhanhtung2002@gmail.com>
2020-05-22 12:10:13 +0700

    Add 2 api to retunr all banks and specific bank

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express')
 
-var serviceAccount = require("../serviceAccountKey.json");
+const app = express()
+
+var serviceAccount = require("./serviceAccountKey.json");
 
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
@@ -12,3 +15,39 @@ admin.initializeApp({
 // As an admin, the app has access to read and write all data, regardless of Security Rules
 const db = admin.firestore();
 
+app.get('/banks', (req, res) => {
+  let banks = [];
+  db.collection('Bank')
+    .get()
+    .then(docs => {
+      docs.forEach(doc => {
+        banks.push({
+          name: doc.data().name,
+          id: doc.id,
+          person: doc.data().person,
+          business: doc.data().business
+        })
+      })
+      res.send(banks)
+    })
+})
+
+app.get('/bank/:id', (req, res) => {
+  const bankId = req.params.id;
+  db.collection('Bank').doc(bankId)
+    .get()
+    .then(doc => {
+      if(!doc.exists) {
+        return res.status(404).json({message: 'Bank not found'})
+      }
+      let bank = doc.data()
+      bank.id = doc.id
+      res.send(bank)
+    })
+})
+
+app.get('/search', (req, res) => {
+  let {object, option, loanType, term} = req.query;
+})
+
+exports.api = functions.https.onRequest(app)
